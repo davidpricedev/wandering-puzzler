2dadeb21722cd4c45b6f75a20eaa6700c5b6b0c5
davidpricedev <david.price.dev@gmail.com>
2023-06-25 14:59:08 -0500

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { runGame } from "./game";
 
 function startGame() {
   const restartButton = document.querySelector("#restart");
-  const levelsButton = document.querySelector("#levels");
   const scoreSpan = document.getElementById("score");
   const canvas = document.querySelector("canvas");
 
@@ -11,10 +10,6 @@ function startGame() {
   canvas.height = canvas.clientHeight;
 
   runGame(canvas, scoreSpan, restartButton);
-  levelsButton.onclick = () => {
-    alert("not implemented yet");
-    // pull up list of levels to pick from
-  };
 }
 
 startGame();
